0930eec8db7427503be5554072a3874ec0198bcd
PromotezCitizen <gkstkdgus821@gmail.com>
2024-12-26 15:24:47 +0900

    feat: mariadb 설정 추가

---
+++ src/config/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mariadb',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USER'),
+          password: configService.get<string>('DB_PASS'),
+          database: configService.get<string>('DB_DATABASE'),
+          synchronize: true,
+          timezone: 'Z',
+          entities: ['dist/**/*.entity.{ts,js}'],
+          logging: true,
+        };
+      },
+    }),
+  ],
+})
+export class DatabaseModule {}
