d29a3fca0adff348758dd1d514c425e71cf0aacd
PromotezCitizen <gkstkdgus821@gmail.com>
2024-12-26 15:44:45 +0900

    remove: 테스트 service 로직 삭제제

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,9 +4,5 @@ import { RedisRepository } from './common/utils/redis.repository';
 @Injectable()
 export class AppService {
   constructor(private readonly redisRepository: RedisRepository) {}
-  async getHello(): Promise<any> {
-    console.log(await this.redisRepository.get('test'));
-    await this.redisRepository.set('test1', '10');
-    return await this.redisRepository.mget(['test', 'test1', 'test2']);
-  }
+  async getHello(): Promise<any> {}
 }
