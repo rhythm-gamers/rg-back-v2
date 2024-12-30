559fdbca3adcd6a27b32098c55d57492baec8d0c
PromotezCitizen <gkstkdgus821@gmail.com>
2024-12-30 16:43:57 +0900

    dependency: steam 의존성 추가

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,13 +6,13 @@ import { User } from './entity/user.entity';
 import { RedisRepository } from 'src/common/utils/redis.repository';
 import { S3BucketService } from 'src/common/utils/s3-bucket.service';
 import { BCryptService } from 'src/common/utils/bcrypt.service';
-import { SteamService } from 'src/steam/steam.service';
 import { FirebaseModule } from 'src/firebase/firebase.module';
+import { SteamModule } from 'src/steam/steam.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), FirebaseModule],
+  imports: [TypeOrmModule.forFeature([User]), FirebaseModule, SteamModule],
   controllers: [UserController],
-  providers: [UserService, RedisRepository, S3BucketService, BCryptService, SteamService],
+  providers: [UserService, RedisRepository, S3BucketService, BCryptService],
   exports: [UserService],
 })
 export class UserModule {}
